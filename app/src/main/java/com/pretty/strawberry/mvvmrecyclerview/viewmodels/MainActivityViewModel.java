52856a88c5139a00749d1fc51acc1dc4185a3845
pritijain <priti.jain@pearson.com>
2019-04-01 10:15:08 -0700

    Whole project adding

---
+++ app/src/main/java/com/pretty/strawberry/mvvmrecyclerview/viewmodels/MainActivityViewModel.java
@@ -0,0 +1,66 @@
+package com.pretty.strawberry.mvvmrecyclerview.viewmodels;
+
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+
+import com.pretty.strawberry.mvvmrecyclerview.models.NicePlace;
+import com.pretty.strawberry.mvvmrecyclerview.repositories.NicePlaceRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends ViewModel {
+
+    private MutableLiveData<List<NicePlace>> mNicePlaces;
+    private NicePlaceRepository mRepo;
+    // determine and represent when query is getting made to some type of data base cache or API
+    private MutableLiveData<Boolean> mIsUpdating = new MutableLiveData<>();
+
+    public void init()
+    {
+        if(mNicePlaces !=null)
+            return;
+        mRepo = NicePlaceRepository.getInstance();
+        mNicePlaces = mRepo.getNicePlaces();
+
+    }
+
+    public void addNewValue(final NicePlace nicePlace)
+    {
+        mIsUpdating.setValue(true);
+
+        new AsyncTask<Void,Void,Void>(){
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                List<NicePlace> currentPlaces = mNicePlaces.getValue();
+                currentPlaces.add(nicePlace);
+                mNicePlaces.postValue(currentPlaces);
+                mIsUpdating.postValue(false);
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Thread.sleep(2000);
+                }catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    public LiveData<List<NicePlace>> getNicePlaces(){
+        return mNicePlaces;
+    }
+
+    public LiveData<Boolean> getIsUpdating()
+    {
+        return mIsUpdating;
+    }
+}
